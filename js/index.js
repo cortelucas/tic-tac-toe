924e8291d105342c5ead892009a0cd697916c367
cortelucas <corte1994.lc@gmail.com>
2021-01-28 15:16:34 -0300

    implementação de atricbuto com sequencias vencedoras

--- js/index.js
+++ js/index.js
@@ -9,7 +9,16 @@ const tic_tac_toe = {
     },
     container_element: null,
     gameover: false,
-
+    winning_sequences: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ],
 
     init: function(container) {
         this.container_element = container;
